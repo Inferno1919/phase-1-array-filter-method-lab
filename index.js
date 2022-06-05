b7ee11f6cd50963f4634f5576716d6482d121b7a
Jake <jakespeitel19@gmail.com>
2022-06-05 19:07:29 -0400

    commit tree c3a4336b8933e751bc2576048c92d35375fbedf4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+
+function findMatching(array, string){
+    return array.filter(name => name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(array, string){
+    
+    return array.filter(name => name.charAt(0) == string.charAt(0));
+
+}
+
+function matchName(array, string){
+
+    return array.filter(dri => dri.name == string );
+}
